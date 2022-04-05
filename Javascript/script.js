e821d6e4e182c7ee16a7e30653eb0c06c4fe7d6e
Merisa <bishwakarma.merisa@gmail.com>
2022-04-05 16:20:41 +0100

    completed functions exercise

--- Javascript/script.js
+++ Javascript/script.js
@@ -89,11 +89,40 @@ for(let i = 1; i <= 10; i++){
 // Next use shift() to remove an element
 // Finally print the contents of the array using a for of loop.
 
-myArray = [`hello`, `everyone`];
-console.log(myArray.length);
-console.log(myArray.push("how", "are", "you"));
-console.log(myArray.shift());
-console.log(myArray);
-for (let eachElement of myArray) {
-    console.log(eachElement);
+// myArray = [`hello`, `everyone`];
+// console.log(myArray.length);
+// console.log(myArray.push("how", "are", "you"));
+// console.log(myArray.shift());
+// console.log(myArray);
+// for (let eachElement of myArray) {
+//     console.log(eachElement);
+// }
+
+
+// FUNCTIONS EXERCISE 
+//Create a function that takes in 2 parameters num1 and num2 and subtracts the two numbers.
+
+function sub(num1, num2) 
+{
+     return num1 - num2;
 }
+
+console.log(sub(10, 5));
+
+// Create a function expression called welcome that take in name, age,gender as a parameters. The outcome should be like so:
+// e.g. "My name is Felix Cited, i am 20 years old and of gender Male"
+
+function welcome(name, age, gender) {
+    console.log(`My name is ` + name + `, i am ` + age + ` years old and of gender ` + gender);
+}
+welcome(`Felix Cited`, 20, `Male`);
+
+
+// Create an arrow function called powerUp that takes in two values n1 and n2. The arrow function will return the power of the two numbers.
+// e.g.
+// n1=2, n2=3 will return 8 (2 x 2 x 2);
+// n1=3, n2=3 will return 27 (3 x 3 x 3);
+// hint use Math.pow()
+
+powerUp = (n1, n2) => Math.pow(n1, n2);
+console.log(powerUp(2,3));
