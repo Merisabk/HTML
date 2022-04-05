3f41f5f78c00962b338035253df85cbe51ba9569
Merisa <bishwakarma.merisa@gmail.com>
2022-04-05 14:10:07 +0100

    Iteration exercises

--- Javascript/script.js
+++ Javascript/script.js
@@ -23,3 +23,27 @@ let model = `A5`;
 console.log(`My Favourtie car is ` + make +` and the model is ` + model + `.`);
 
 console.log(`%c` + `This is a red text`, `color:red; font-family:fantasy; font:bold; background-color:black, padding: 10px;`);
+
+
+//ITERATION EXERCISE
+let A = 100;
+for (let A = 100; A<=200; A++) {
+    A++;
+    console.log(A++);
+}
+
+let B = 100;
+for (let B = 100; B <= 200; B++) {
+    if (B % 2 == 0){
+        console.log(`-`);
+    } else {
+        console.log(`*`);
+    }
+    }
+
+// create a method that can print out the numbers 1-10 10 times each 
+for(let i = 1; i <= 10; i++){
+    for (let j=1; j <=10; j++)
+    console.log(j);
+}
+
