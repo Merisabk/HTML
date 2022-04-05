e7cf4cd46c56642fcca72972d89c7d0a46df265e
Merisa <bishwakarma.merisa@gmail.com>
2022-04-05 11:06:41 +0100

    completed JS exercises

---
+++ Javascript/script.js
@@ -0,0 +1,25 @@
+// USE STRICT and CONSOLE
+// use strict: forces us to write secure javaScript. stops us using protected words, accidentaly making global variables etc. 
+//`use strict` 
+let myVar =1;
+
+console.log(`Hello World`);
+console.info(`this is info`);
+console.warn(`this is a warning`);
+console.error(`this is an error msd`);
+console.log(`%c` + `some text`, `color:black; background-color:white; font-size:30px; padding:15px;`);
+
+console.log(`%c` + `red text` + `%c` + `blue text`, `color:red;`, `color:blue; `);
+
+// Exercises
+console.log(`firstname`);
+console.log(`secondname`);
+console.log(`where are you from?`);
+console.log(`star sign`);
+
+let make = `Audi`;
+let model = `A5`;
+
+console.log(`My Favourtie car is ` + make +` and the model is ` + model + `.`);
+
+console.log(`%c` + `This is a red text`, `color:red; font-family:fantasy; font:bold; background-color:black, padding: 10px;`);
